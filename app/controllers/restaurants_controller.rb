cd35e6bb18b62419cffc3f1899b86df7bcbd1807
Tarig Bouazzati <tbouzza@gmail.com>
2021-02-22 10:43:25 +0400

    added bootstrap

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+
+    redirect_to @restaurant
+  end
+
+  def destroy
+    @restaurant.delete
+
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get '/restaurants', to: 'restaurants#index'
+
+  # Create a restaurant
+  #get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  #post '/restaurants', to: 'restaurants#create'
+
+  # Show one restaurant
+  #get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Edit a restaurant
+  #get '/restaurants/:id/edit', to: 'restaurants#edit'
+  #patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Delete a restaurant
+  #delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
 end
